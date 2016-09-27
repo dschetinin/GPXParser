26e597ef72613b7483a220240955f681d0681499
Dmitry Schetinin <schetinin.d@gmail.com>
2016-09-27 17:45:35 +0300

    Java lambdas added, log4j configuration tuned

--- src/main/java/com/gpxparser/SrtFileReaderLambda.java
+++ src/main/java/com/gpxparser/SrtFileReaderLambda.java
@@ -8,62 +8,52 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Calendar;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Locale;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.ZoneId;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * User: Dima <a href="mailto:schetinin.d@gmail.com"/>schetinin.d@gmail.com</a>
- * Date: 9/10/2016
- * Time: 8:55 PM
+ * User: dschetinin <a href="mailto:schetinin.d@gmail.com"/>schetinin.d@gmail.com</a>
+ * Date: 9/27/2016
+ * Time: 4:06 PM
  * To change this template use File | Settings | File Templates.
  */
-public class SrtFileReader {
-    private static final Logger logger = LogManager.getLogger(SrtFileReader.class);
+public class SrtFileReaderLambda extends SrtFileReader {
+    private static final Logger logger = LogManager.getLogger(SrtFileReaderLambda.class);
 
-    protected static String IN_FILENAME = "C:\\GIT_Projects\\GPXParser\\files\\DJI_0172.SRT";
-
-    /*
-
-    **** Subtitle text fragment format ****
-
-    1
-    00:00:01,000 --> 00:00:02,000
-    HOME(30.4473,50.3637) 2016.02.25 16:12:59
-    GPS(30.4451,50.3657,19) BAROMETER:88.1
-    ISO:100 Shutter:500 EV: 0 Fnum:F2.8
-
-    2
-    00:00:02,000 --> 00:00:03,000
-    HOME(30.4473,50.3637) 2016.02.25 16:13:00
-    GPS(30.4451,50.3657,19) BAROMETER:88.2
-    ISO:100 Shutter:500 EV: 0 Fnum:F2.8
-
-    */
-
-    protected Pattern numberLinePattern = Pattern.compile("^[\\d]+$");
-    protected Pattern gpsLinePattern = Pattern.compile("^GPS\\((\\d{2}\\.\\d{4}),(\\d{2}\\.\\d{4}),(\\d{2}).*BAROMETER:(\\d{2,3}\\.\\d)$");
-    protected Pattern dateLinePattern = Pattern.compile("^.*(\\d{4})\\.(\\d{2})\\.(\\d{2})\\s(\\d{2}):(\\d{2}):(\\d{2}).*$");
-
+    protected Pattern emptyStringPattern = Pattern.compile("^\\n|\\r|\\r\\n|\\u0085|\\u2028|\\u2029$");
 
     public static void main (String [] args) {
+        SrtFileReaderLambda fileReaderLambda = new SrtFileReaderLambda();
         SrtFileReader fileReader = new SrtFileReader();
-        fileReader.getPointListFromSrtFile(IN_FILENAME);
+
+//        fileReader.getPointListFromSrtFile(IN_FILENAME);
+        fileReaderLambda.getPointListFromSrtFile(IN_FILENAME);
     }
 
-    protected List<SrtDataBlock> getPointListFromSrtFile (String filePath) {
+    @Override
+    protected List<SrtDataBlock> getPointListFromSrtFile(String filePath) {
+        List<String> fileStringList = new ArrayList<>();
         LinkedList<SrtDataBlock> srtPointList = new LinkedList<SrtDataBlock>();
 
         try (FileInputStream fis = new FileInputStream(filePath);
              BufferedReader reader = new BufferedReader(new InputStreamReader(fis))) {
+            // first build a list of a string objects
+            String text = reader.readLine();
+            while (text != null) {
+                fileStringList.add(text);
+                // read the next line
+                text = reader.readLine();
+            }
 
-            String line = reader.readLine();
-            while (line != null) {
-                // do action with line
-                logger.info(line);
+            // now let's use stream/lambdas to build another list
+            fileStringList.stream().filter(line -> {
+                Matcher matcher = emptyStringPattern.matcher(line);
+                return !matcher.matches() && !"".equals(line);
+            }).forEach(line -> {
                 Matcher matcher = numberLinePattern.matcher(line);
                 SrtDataBlock srtBlock = null;
                 // determine whether new block is starting.
@@ -72,11 +62,9 @@ public class SrtFileReader {
                     srtBlock = new SrtDataBlock();
                     srtBlock.setBlockNumber(Integer.parseInt(line));
                     srtPointList.addLast(srtBlock);
-
                 }
-                // extract and build GPS longitude, latitude
 
-                // extract and build GPS elevation data
+                // extract and build GPS longitude, latitude and elevation data
                 matcher = gpsLinePattern.matcher(line);
                 if (matcher.matches()) {
                     Double longitude = Double.parseDouble(matcher.group(1));
@@ -94,25 +82,21 @@ public class SrtFileReader {
                 matcher = dateLinePattern.matcher(line);
                 if (matcher.matches()) {
                     int year = Integer.parseInt(matcher.group(1));
-                    int month = Integer.parseInt(matcher.group(2)) - 1; // Month is 0 based according to calendar documentation
+                    int month = Integer.parseInt(matcher.group(2)); // month is from 1 to 12 according to LocaDateTime API
                     int day = Integer.parseInt(matcher.group(3));
                     int hour = Integer.parseInt(matcher.group(4));
                     int minute = Integer.parseInt(matcher.group(5));
                     int second = Integer.parseInt(matcher.group(6));
 
-                    Calendar cal = Calendar.getInstance(Locale.getDefault());
-                    cal.set(year, month, day, hour, minute, second);
+                    LocalDateTime dateTime = LocalDateTime.of(year, Month.of(month), day, hour, minute, second);
+                    Date date = new Date(dateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
+
                     srtBlock = srtPointList.getLast();
-                    srtBlock.setTime(cal.getTime());
+                    srtBlock.setTime(date);
                 }
-                // read the next line
-                line = reader.readLine();
-            }
+            });
 
-            for (SrtDataBlock element : srtPointList) {
-                logger.debug(element);
-            }
-
+            srtPointList.forEach(element -> logger.debug(element));
             logger.info("Size of result LinkedList is : " + srtPointList.size());
 
         } catch (IOException e) {
@@ -120,5 +104,4 @@ public class SrtFileReader {
         }
         return srtPointList;
     }
-
 }
